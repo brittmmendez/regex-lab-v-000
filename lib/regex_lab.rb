4f6dc8632ca9e87f9a72aa553c9d05c2c28bd2f6
Brittany Morris <brittmmendez@gmail.com>
2017-12-06 16:12:07 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -16,7 +16,10 @@ text.scan(/\b\w{5}\b/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-text.match(/^[aeiou].*[!.,:;']$/)
+if text.match(/^[aeiou].*[!.,:;']$/)
+return true
+else
+  return false
 end
 
 def valid_phone_number?(phone)
